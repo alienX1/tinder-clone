8fe283f8a56d29c2f808205b4b25acc94b118f79
AlienX <aditya1115.ar@gmail.com>
2021-12-24 08:44:47 +0530

    initialise

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -4,52 +4,52 @@ import './TinderCards.css';
 import axios from '../axios'
 
 function TinderCards() {
-    // const [people, setPeople] = useState([
-    // {
-    //     name: 'Joginder',
-    //     photo: '/images/pic1.jpg'
-    // },
-    // {
-    //     name: 'Narender',
-    //     photo: "/images/pic2.jpg"
-    // },
-    // {
-    //     name: 'Surender',
-    //     photo: "/images/pic3.jpg"
-    // },
-    // {
-    //     name: 'Thara Bhai',
-    //     photo: "/images/pic4.jpg"
-    // },
-    // {
-    //     name: 'Jenny',
-    //     photo: "/images/pic5.jpg"
-    // },
-    // {
-    //     name: 'Jagga',
-    //     photo: "/images/pic6.jpg"
-    // },
-    // {
-    //     name: 'Jugna',
-    //     photo: "/images/pic7.jpg"
-    // },
-    // {
-    //     name: 'Jugni',
-    //     photo: "/images/pic8.jpg"
-    // },
-    // ]);
+    const [people, setPeople] = useState([
+    {
+        name: 'Joginder',
+        photo: '/images/pic1.jpg'
+    },
+    {
+        name: 'Narender',
+        photo: "/images/pic2.jpg"
+    },
+    {
+        name: 'Surender',
+        photo: "/images/pic3.jpg"
+    },
+    {
+        name: 'Thara Bhai',
+        photo: "/images/pic4.jpg"
+    },
+    {
+        name: 'Jenny',
+        photo: "/images/pic5.jpg"
+    },
+    {
+        name: 'Jagga',
+        photo: "/images/pic6.jpg"
+    },
+    {
+        name: 'Jugna',
+        photo: "/images/pic7.jpg"
+    },
+    {
+        name: 'Jugni',
+        photo: "/images/pic8.jpg"
+    },
+    ]);
 
-    const [people, setPeople] = useState([]);
+    // const [people, setPeople] = useState([]);
 
-    useEffect(() => {
-        async function fetchData () {
-            const req = await axios.get('/tinder/cards');
+    // useEffect(() => {
+    //     async function fetchData () {
+    //         const req = await axios.get('/tinder/cards');
 
-            setPeople(req.data);
-        }
+    //         setPeople(req.data);
+    //     }
 
-        fetchData();
-    }, [])
+    //     fetchData();
+    // }, [])
 
     const swiped = (direction, nameToDelete) => {
         console.log('removing: ' + nameToDelete);
